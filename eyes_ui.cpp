0eb99dd72bb32ae51787f498aeb0fb1cd6a4c63e
Krzysztof Mędrzycki <iphoeenix@gmail.com>
2010-12-11 22:03:08 +0100

    dodalem morzliwość wyboru coloru

--- eyes_ui.cpp
+++ eyes_ui.cpp
@@ -1,5 +1,7 @@
 #include <QtGui>
 #include <QApplication>
+#include <QDebug>
+#include <QIODevice>
 
 #include "eyes_view.h"
 #include "eyes_info.h"
@@ -13,6 +15,26 @@ int main ( int argc, char ** argv )
   win.setWindowTitle( "!Eyesy" );
   win.setAttribute ( Qt::WA_TranslucentBackground, true );
   win.show ();
+
+  //QIODevice _iod = new QIODevice ();
+  //QDebug dbg = new QDebug( _iod );
+
+  QStringList arg = app.arguments ();
+  QString folder ( "green" );
+  for ( int i = 0 ; i < arg.size () ; i++ )
+  {
+      if ( arg.at ( i ) == "--color" )
+      {
+          folder = arg.at ( i ).split ( "=" )[1];
+      }
+      else if ( arg.at ( i ) == "-c" )
+      {
+          i++;
+          folder = arg.at ( i );
+      }
+  }
+
+  qDebug () << folder;
 
   eyes_view eyes ( &win );
   eyes.show();
