255d3a36fbf4c9816b1959de5faffaa7dcdfbcbc
Krzysztof Mędrzycki <iphoeenix@gmail.com>
2010-12-11 21:36:28 +0100

    dodalem przeźroczystość okna

--- eyes_ui.cpp
+++ eyes_ui.cpp
@@ -1,7 +1,21 @@
-#include <QtGui/qapplication.h>
+#include <QtGui>
+#include <QApplication>
+
+#include "eyes_view.h"
+#include "eyes_info.h"
 
 int main ( int argc, char ** argv )
 {
   QApplication app ( argc, argv );
+
+  QWidget win;
+  win.resize ( EYES_W, EYES_H );
+  win.setWindowTitle( "!Eyesy" );
+  win.setAttribute ( Qt::WA_TranslucentBackground, true );
+  win.show ();
+
+  eyes_view eyes ( &win );
+  eyes.show();
+
   return app.exec ();
 }
