8b7528c40604754654936f089c8c0f6834d01aa4
Krzysztof Mędrzycki <iphoeenix@gmail.com>
2010-12-13 18:33:43 +0100

    działa wyświetlanie obrazków, ALE TYLKO kiedy binarka jest w folderze z sourcem

--- eyes_view.cpp
+++ eyes_view.cpp
@@ -1,9 +1,18 @@
 #include "eyes_view.h"
 #include "eyes_info.h"
 
+#include <QPainter>
+
+#include <iostream>
+
+#define MM_NO_MOTION 10000
+
 eyes_view::eyes_view ( QWidget * parent ) : QWidget ( parent )
 {
     g_scene = new QGraphicsScene ( this );
+    bulwers = normal;
+    px = MM_NO_MOTION;
+    px = MM_NO_MOTION;
     setMinimumSize ( EYES_W, EYES_H );
     setMaximumSize ( EYES_W, EYES_H );
 }
@@ -15,4 +24,35 @@ eyes_view::~eyes_view ()
 
 void eyes_view::open_images ( QString folder )
 {
+    images.push_back ( QPixmap ( folder+"/#320_eye_std_01.png" ) );
+    if ( images[0].isNull () )
+    {
+        std::cout << "To jest nUlL ;(\n";
+    }
+}
+
+void eyes_view::paintEvent ( QPaintEvent * event )
+{
+    QPainter paint ( this );
+    paint.drawPixmap ( 0, 0, EYES_W, EYES_H, images[0] );
+    paint.drawText ( 0, 0, "Ala ma kota!" );
+}
+
+void eyes_view::mousePressEvent ( QMouseEvent * ev )
+{
+    ev->ignore ();
+}
+
+void eyes_view::mouseMoveEvent ( QMouseEvent * ev )
+{
+    ev->accept ();
+    if ( px == MM_NO_MOTION or py == MM_NO_MOTION )
+    {
+        px = ev->x ();
+        py = ev->y ();
+        return;
+    }
+    int dx = ( ( px > ev->x () ? -1 : 1 ) * px ) + ev->x ();
+    int dy = ( ( py > ev->y () ? -1 : 1 ) * py ) + ev->y ();
+    move ( dx, dy );
 }
--- eyes_view.h
+++ eyes_view.h
@@ -4,7 +4,9 @@
 #include <QWidget>
 #include <QGraphicsScene>
 #include <QImage>
+#include <QPixmap>
 #include <QVector>
+#include <QMouseEvent>
 
 #include "bulwers.h"
 
@@ -12,15 +14,20 @@ class eyes_view : public QWidget
 {
     Q_OBJECT
 public:
-    explicit    eyes_view   ( QWidget * parent );
-                ~eyes_view  ();
-    void        open_images ( QString folder );
+    explicit    eyes_view       ( QWidget * parent );
+                ~eyes_view      ();
+    void        open_images     ( QString folder );
+    void        paintEvent      ( QPaintEvent * );
 signals:
 public slots:
+        void    mousePressEvent ( QMouseEvent * ev );
+        void    mouseMoveEvent  ( QMouseEvent * ev );
 private:
-    QGraphicsScene  * g_scene;
-    QVector <QImage>  images;
-    bulwers_enum      bulwers;
+    QGraphicsScene    * g_scene;
+    QVector <QPixmap>   images;
+    bulwers_enum        bulwers;
+    int                 px,
+                        py;
 };
 
 #endif // EYES_VIEW_H
