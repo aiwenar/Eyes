3f6b41272b86c88633f3c71f3d7eb7ed67388c3e
Lapsio <lapsio@LaP3k.(none)>
2010-12-15 20:57:49 +0100

    huashasha

---
+++ HARDWARE.h
@@ -0,0 +1,17 @@
+
+#ifndef HARDWARE
+#define
+
+#include <glibtop.h>
+#include <glibtop/cpu.h>
+#include <glibtop/mem.h>
+#include <glibtop/proclist.h>
+
+
+
+int C_LOAD ();
+int M_LOAD ();
+int U_TIME ();
+int P_LIST ();
+
+#endif
