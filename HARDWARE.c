d0f51d658817bbddfab4eac39f5dabbc73968c79
Lapsio <lapsio@LaP3k.(none)>
2010-12-15 21:46:46 +0100

    niehuashasha

--- HARDWARE.c
+++ HARDWARE.c
@@ -8,19 +8,19 @@ int U_TIME ();
 //---------------------------
 int C_LOAD ()
 {
-glibtop_init();
-glibtop_cpu cpu;
-glibtop_get_cpu (&cpu);
+	glibtop_init();
+	glibtop_cpu cpu;
+	glibtop_get_cpu (&cpu);
 
-static unsigned short cpu_load = 0;
-static unsigned long p_idle = 0;
-static unsigned long p_total = 0;
-static unsigned long a_idle = 0;
-static unsigned long a_total = 0;
-static unsigned short d_total = 0;
-static unsigned short d_idle = 0;
-a_idle = cpu.idle;
-a_total = cpu.total;
+	static unsigned short cpu_load = 0;
+	static unsigned long p_idle = 0;
+	static unsigned long p_total = 0;
+	static unsigned long a_idle = 0;
+	static unsigned long a_total = 0;
+	static unsigned short d_total = 0;
+	static unsigned short d_idle = 0;
+	a_idle = cpu.idle;
+	a_total = cpu.total;
 
         d_total = a_total - p_total;
         d_idle = a_idle - p_idle;
@@ -28,38 +28,38 @@ a_total = cpu.total;
 	p_idle = a_idle;
 	p_total = a_total;
 
-return cpu_load;
+	return cpu_load;
 } 
 //-------------------------
 int M_LOAD ()
 {
-glibtop_init();
-glibtop_mem memory;
-glibtop_get_mem(&memory);
-
-unsigned int mem_total = memory.total;
-unsigned int mem_used = memory.used;
-unsigned int mem_load = (100*mem_used)/mem_total;
-
-return mem_load;
+	glibtop_init();
+	glibtop_mem memory;
+	glibtop_get_mem(&memory);
+	
+	unsigned int mem_total = memory.total;
+	unsigned int mem_used = memory.used;
+	unsigned int mem_load = (100*mem_used)/mem_total;
+	
+	return mem_load;
 }
 //-------------------------
 int P_LIST ()
 {
-int which,arg;
-
-glibtop_init();
-glibtop_proclist proclist;
-glibtop_get_proclist(&proclist,which,arg);
-
-return proclist.number;
+	int which,arg;
+	
+	glibtop_init();
+	glibtop_proclist proclist;
+	glibtop_get_proclist(&proclist,which,arg);
+	
+	return proclist.number;
 }
 //-------------------------
 int U_TIME ()
 {
-glibtop_init();
-glibtop_uptime uptime;
-glibtop_get_uptime(&uptime);
+	glibtop_init();
+	glibtop_uptime uptime;
+	glibtop_get_uptime(&uptime);
 
-return uptime.uptime;
+	return uptime.uptime;
 }
