10b7b1b4654ab3b32f040e28863e9277bf627dc7
Lapsio <lapsio@LaP3k.(none)>
2010-12-26 21:27:57 +0100

    huashasha

--- optional.cpp
+++ optional.cpp
@@ -4,7 +4,7 @@
 #include </usr/include/qt4/Qt/qstring.h>
 #include </usr/include/qt4/Qt/qvector.h>
 #include </usr/include/qt4/Qt/qregexp.h>
-#include <qstringlist.h>
+#include </usr/include/qt4/Qt/qstringlist.h>
 #include <cstdlib>
 #include <unistd.h>
 #include <fstream>
@@ -32,8 +32,21 @@ int main ()
 {
     int i=0;
     while (i<7){
-    cout <<get_time ().day << endl << get_time ().day_num <<
-            endl << get_time().hour << endl << get_time().month << endl;
+    cout << "day"
+            << get_time ().day
+            << endl
+         << "dnum"
+            << get_time ().day_num
+            << endl
+         << "hour"
+            << get_time().hour
+            << endl
+         << "month"
+            << get_time().month
+            << endl
+         << "year"
+            << get_time().year
+            << endl;
     //cout << endl << "temp " << temperatura () << endl << bat_plugged () << " " << bateria << endl;
 i++;
 sleep (1);
@@ -53,13 +66,13 @@ sdate get_time ()
     sdate get_date;
     QString tmp1 = asctime ( localtime ( &timer ) );
     QStringList l_date ( tmp1.split ( ' ' ) );
-    if ( l_date[1] == "Mon" )get_date.day = 1;
-    if ( l_date[1] == "Tue" )get_date.day = 2;
-    if ( l_date[1] == "Wed" )get_date.day = 3;
-    if ( l_date[1] == "Thu" )get_date.day = 4;
-    if ( l_date[1] == "Fri" )get_date.day = 5;
-    if ( l_date[1] == "Sat" )get_date.day = 6;
-    if ( l_date[1] == "Sun" )get_date.day = 7;
+    if ( l_date[0] == "Mon" )get_date.day = 1;
+    if ( l_date[0] == "Tue" )get_date.day = 2;
+    if ( l_date[0] == "Wed" )get_date.day = 3;
+    if ( l_date[0] == "Thu" )get_date.day = 4;
+    if ( l_date[0] == "Fri" )get_date.day = 5;
+    if ( l_date[0] == "Sat" )get_date.day = 6;
+    if ( l_date[0] == "Sun" )get_date.day = 7;
 
     if ( l_date[1] == "Jan" )get_date.month = 1;
     if ( l_date[1] == "Feb" )get_date.month = 2;
@@ -74,10 +87,10 @@ sdate get_time ()
     if ( l_date[1] == "Nov" )get_date.month = 11;
     if ( l_date[1] == "Dec" )get_date.month = 12;
 
-    get_date.day_num = (int)l_date[3];
-    QStringList tmp2 ( l_date[4].split ( ':' ) );
+    get_date.day_num = l_date[2].toInt ();
+    QStringList tmp2 ( l_date[3].split ( ':' ) );
     get_date.hour = ( 3600 * ( tmp2[0].toInt () ) + ( 60 * ( tmp2[1].toInt() ) ) + tmp2[2].toInt () );
-    get_date.year = l_date[5].toInt ();
+    get_date.year = l_date[4].toInt ();
 
     return get_date;
 }
