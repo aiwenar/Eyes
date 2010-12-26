f631f9428e99d67f454cef37de799f33d7c07171
Krzysztof Mędrzycki <iphoeenix@gmail.com>
2010-12-26 20:34:14 +0100

    moze zadziala...

--- optional.cpp
+++ optional.cpp
@@ -4,6 +4,7 @@
 #include </usr/include/qt4/Qt/qstring.h>
 #include </usr/include/qt4/Qt/qvector.h>
 #include </usr/include/qt4/Qt/qregexp.h>
+#include <qstringlist.h>
 #include <cstdlib>
 #include <unistd.h>
 #include <fstream>
@@ -51,7 +52,8 @@ sdate get_time ()
 
     sdate get_date;
     QString tmp1 = asctime ( localtime ( &timer ) );
-    QStringList l_date = tmp1.split ( ' ' );
+    QStringList l_date = new QStringList ();
+    l_date = tmp1.split ();
     if ( l_date[1] == "Mon" )get_date.day = 1;
     if ( l_date[1] == "Tue" )get_date.day = 2;
     if ( l_date[1] == "Wed" )get_date.day = 3;
@@ -74,7 +76,8 @@ sdate get_time ()
     if ( l_date[1] == "Dec" )get_date.month = 12;
 
     get_date.day_num = (int)l_date[3];
-    QStringList tmp2 = l_date[4].split ( ':' );
+    QStringList tmp2 = new QStringList ();
+    tmp2 = l_date[4].split ( ':' );
     get_date.hour = (3600*((int)tmp2[0]) + (60*((int)tmp2[1])) + ((int)tmp2[2]));
     get_date.year = (int)l_date[5];
 
