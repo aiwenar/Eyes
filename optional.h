7f59925afc8b11bee993715fccbf7e4a038ee559
Lapsio <lapsio@LaP3k.(none)>
2010-12-26 21:46:32 +0100

    huashasha

---
+++ optional.h
@@ -0,0 +1,23 @@
+
+#ifndef OPTIONAL
+#define
+
+#include <string.h>
+#include <time.h>
+#include </usr/include/qt4/Qt/qstring.h>
+#include </usr/include/qt4/Qt/qvector.h>
+#include </usr/include/qt4/Qt/qregexp.h>
+#include </usr/include/qt4/Qt/qstringlist.h>
+#include <cstdlib>
+#include <unistd.h>
+#include <fstream>
+#include <cmath>
+
+
+
+sdate get_time ();
+int temperatura ();
+int bat_plugged ();
+int bateria ();
+
+#endif
