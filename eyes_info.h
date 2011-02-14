15ad7030d83ea941ba656592bfcd109e7e2164c6
Krzysztof Mędrzycki <iphoeenix@gmail.com>
2011-02-14 20:23:10 +0100

    kosmetyka :)

--- eyes_info.h
+++ eyes_info.h
@@ -1,5 +1,5 @@
-#ifndef EYES_INFO_H
-#define EYES_INFO_H
+#ifndef _eyes_eyes_info_h
+#define _eyes_eyes_info_h
 
 #define EYE_S   60
 #define EYE_M   9
@@ -7,4 +7,4 @@
 #define EYES_W  320
 #define EYES_H  80
 
-#endif // EYES_INFO_H
+#endif //eyes_eyes_info_h
