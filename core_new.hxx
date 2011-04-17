804185dcd646b627caa10e3600ebee789039fb03
lapsio <lapsio@linux-v89o.site>
2011-04-17 20:25:22 +0200

    drobne poprawki (huashasha)

--- core_new.hxx
+++ core_new.hxx
@@ -14,33 +14,33 @@ using namespace libconfig;
 
 struct sdate
 {
-    unsigned short day,
-                   month,
-                   day_num;
-    unsigned int   hour,
-                   year;
+    unsigned short              day,
+                                month,
+                                day_num;
+    unsigned int                hour,
+                                year;
 };
 
 class percental
 {
 public:
-    short                   mod,
-                            mod_prev;
-    vector <double>         probes,
-                            sector_small;
-    double                  load,
-                            stable;
-    unsigned int            buff_size,
-                            steps,
-                            lin_num,
-                            current_probe,
-                            current_probe_small,
-                            loseless,
-                            calculate();
-    char                    frequency;
-    void                    get_load (double),
-                            init (QString adress);
-    bool                    buffered;
+    short                       mod,
+                                mod_prev;
+    vector <double>             probes,
+                                sector_small;
+    double                      load,
+                                stable;
+    unsigned int                buff_size,
+                                steps,
+                                lin_num,
+                                current_probe,
+                                current_probe_small,
+                                loseless,
+                                calculate();
+    char                        frequency;
+    void                        get_load (double),
+                                init (QString adress);
+    bool                        buffered;
 };
 
 class unital
@@ -69,59 +69,69 @@ public:
 class timal
 {
 public:
-    short               mod;
-    unsigned int        value,
-                        start,
-                        steps,
-                        lin_num,
-                        wide,
-                        end,
-                        calculate();
-    char                frequency;
-    void                init (QString adress);
+    short                       mod;
+    unsigned int                value,
+                                start,
+                                steps,
+                                lin_num,
+                                wide,
+                                end,
+                                calculate();
+    char                        frequency;
+    void                        init (QString adress);
 };
 
 class bul
 {
 public:
-    int                     total_mod;
-    unsigned int            step,
-                            wall_01,
-                            wall_02,
-                            wall_03,
-                            wall_04,
-                            wall_05,
-                            wall_06,
-                            wall_07,
-                            wall_08,
-                            wall_09,
-                            wall_10,
-                            wall_11,
-                            wall_12,
-                            wall_13,
-                            wall_14,
-                            wall_15;
-    unsigned short          outline,
-                            prev_outline,
-                            eye,
-                            tired,
-                            hot,
-                            shy,
-                            layer2,
-                            layer3,
-                            layer4,
-                            value;
-    void                    update();
+    int                         total_mod;
+    unsigned int                step,
+                                wall_01,
+                                wall_02,
+                                wall_03,
+                                wall_04,
+                                wall_05,
+                                wall_06,
+                                wall_07,
+                                wall_08,
+                                wall_09,
+                                wall_10,
+                                wall_11,
+                                wall_12,
+                                wall_13,
+                                wall_14,
+                                wall_15;
+    unsigned short              outline,
+                                prev_outline,
+                                eye,
+                                tired,
+                                hot,
+                                shy,
+                                layer2,
+                                layer3,
+                                layer4,
+                                value;
+    void                        update();
+};
+class hard_dbg
+{
+private:
+    unsigned short              line    ,
+                                spacer  ;
+    void                        chck_s();
+public:
+    unsigned short              max_s   ;
+    void                        print() ;
 };
 
 struct sended_anims
 {
-    QString                 close,
-                            open,
-                            eyes,
-                            face_prev;
-    unsigned short          start,
-                            end;
+    QString                     close,
+                                open,
+                                eyes,
+                                face_prev;
+    unsigned short              start,
+                                end;
 };
 
 #endif //gbssl_core_new_hxx
