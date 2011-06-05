13bd5ea6881d3cbfbc84ca515b463030c04fded3
phoeniix <iphoeenix@gmail.com>
2011-06-05 21:14:35 +0200

    initial commit of eyes.hxx;

---
+++ eyes.hxx
@@ -0,0 +1,8 @@
+#ifndef _eyes_eyes_hxx
+#define _eyes_eyes_hxx
+
+//version double format: a.bc.de z (xy when x=0 == y) z=[a=0/b=1/f=2] is a.bcde0z
+const double version = 0.080001;
+const char *verstr = "0.8 alpha";
+
+#endif //eyes_eyes_hxx
