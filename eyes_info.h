498aa207f4293a07882e6da5b003f13e05fcda96
Krzysztof Mędrzycki <iphoeenix@gmail.com>
2011-10-08 13:20:19 +0200

    moved to eyes_view.hxx

--- eyes_info.h
+++ eyes_info.h
@@ -18,15 +18,4 @@
 #ifndef _eyes_eyes_info_h
 #define _eyes_eyes_info_h
 
-#define EYE_S   60
-#define EYE_M   9
-
-extern int  eye_s,
-            eye_m,
-            eyes_w,
-            eyes_h;
-
-#define EYES_W  320
-#define EYES_H  80
-
 #endif //eyes_eyes_info_h
