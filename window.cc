84220c7c888e306669f56b7496933d6d692474a0
Krzysztof Mędrzycki <iphoeenix@gmail.com>
2011-10-08 14:05:51 +0200

    include fixes

--- window.cc
+++ window.cc
@@ -17,7 +17,7 @@
 
 #include <iostream>
 #include <QApplication>
-#include "eyes_window.hxx"
+#include "window.hxx"
 #include "eyes.hxx"
 #include <debug.hxx>
 
@@ -26,7 +26,6 @@ using namespace std;
 eyes_window::eyes_window ( QString color, QWidget * parent ) : QWidget ( parent, Qt::WindowStaysOnTopHint | Qt::FramelessWindowHint )
 {
     info << "(eyes_window) preparing...\n";
-    oldcfg = new eConfig ( "config.cfg" );
     Configuration * cfg = Configuration::getInstance ();
     eyes_w = cfg->lookupValue ( "ui.window.size.width", 320 );
     eyes_h = cfg->lookupValue ( "ui.window.size.height", 80 );
@@ -36,9 +35,7 @@ eyes_window::eyes_window ( QString color, QWidget * parent ) : QWidget ( parent,
     setAttribute ( Qt::WA_TranslucentBackground, true );
     //setWindowFlags ( Qt::SplashScreen );
     eyes = new eyes_view ( this, color );
-    ecfg = new eyes_config ( *oldcfg, (QWidget*)nil );
     config = new Configurator ( (QWidget*)nil );
-    ecfg->set_icon ( eyes->get_color_suffix () );
     info << "(eyes_window) loading icon " << ( QString ( "./pics/icon" ) + eyes->get_color_suffix () + ".png" ).toStdString () << ".\n";
     QPixmap tmp;
     tmp.load ( QString ( "./pics/icon" ) + eyes->get_color_suffix () + ".png" );
